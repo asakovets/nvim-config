b90a3ef65563edf3e7d8d2a7e2c86a7f8b332ac9
Aleksey Sakovets <alexeysakovets@gmail.com>
2025-06-25 10:46:03 +0300

    Disable neovide animations

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,8 +36,13 @@ if vim.g.neovide then
     vim.g.neovide_opacity = 0.95
     vim.g.neovide_background_color = "#eeeeee"
     vim.g.neovide_theme = 'light'
-    -- vim.g.neovide_cursor_animation_length = 0
-    -- vim.g.neovide_scroll_animation_length = 0
+    vim.g.neovide_position_animation_length = 0
+    vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_cursor_trail_size = 0
+    vim.g.neovide_cursor_animate_in_insert_mode = false
+    vim.g.neovide_cursor_animate_command_line = false
+    vim.g.neovide_scroll_animation_far_lines = 0
+    vim.g.neovide_scroll_animation_length = 0
 end
 
 
