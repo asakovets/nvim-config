7f9accdaab3ee8c1ce1ad625f3be507f17a1c92d
Aleksey Sakovets <alexeysakovets@gmail.com>
2025-08-24 10:02:06 +0300

    almost-mono: adjust terminal colors

--- colors/almost-mono.lua
+++ colors/almost-mono.lua
@@ -1,6 +1,6 @@
 -- You probably always want to set this in your vim file
-vim.opt.background = 'light'
-vim.g.colors_name = 'almost-mono'
+vim.opt.background = "light"
+vim.g.colors_name = "almost-mono"
 
 -- By setting our module to nil, we clear lua's cache,
 -- which means the require ahead will *always* occur.
@@ -14,7 +14,27 @@ vim.g.colors_name = 'almost-mono'
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded['lush_theme.almost-mono'] = nil
+package.loaded["lush_theme.almost-mono"] = nil
 
 -- include our theme file and pass it to lush to apply
-require('lush')(require('lush_theme/almost-mono'))
+require ("lush") (require ("lush_theme/almost-mono"))
+
+-- Return our parsed theme for extension or use elsewhere.
+vim.g.terminal_color_0 = "#000000" -- black
+vim.g.terminal_color_1 = "#DC322F" -- red
+vim.g.terminal_color_2 = "#009908" -- green
+vim.g.terminal_color_3 = "#B58900" -- yellow
+vim.g.terminal_color_4 = "#268BD2" -- blue
+vim.g.terminal_color_5 = "#D33682" -- magenta
+vim.g.terminal_color_6 = "#2AA198" -- cyan
+vim.g.terminal_color_7 = "#000000" -- white
+
+-- All bright colors dimmed to black :
+vim.g.terminal_color_8 = "#000000"
+vim.g.terminal_color_9 = "#CB4B16"
+vim.g.terminal_color_10 = "#586E75"
+vim.g.terminal_color_11 = "#3c3f40"
+vim.g.terminal_color_12 = "#839496"
+vim.g.terminal_color_13 = "#6C71C4"
+vim.g.terminal_color_14 = "#93A1A1"
+vim.g.terminal_color_15 = "#000000"
